edafd55f7bbac213f23508a39ca89debe5fca7a0
Akash Raj <akashraj7713@gmail.com>
2022-06-23 13:59:13 +0530

    Time: 36 ms (78.29%), Space: 14 MB (12.53%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.py
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.py
@@ -6,13 +6,37 @@
 #         self.right = right
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        #Moris traversal
+        def findPred(curr):
+            node = curr.left
+            while node.right and node.right!= curr:
+                node = node.right
+            return node
+            
+        curr =root
         res = []
-        def preorder(root):
-            if root:
-                res.append(root.val)
-                preorder(root.left)
-                preorder(root.right)                
-                
-        preorder(root)
+        while curr:
+            if curr.left is None:
+                res.append(curr.val)
+                curr = curr.right
+            else:
+                pred = findPred(curr)
+                if pred.right is None:
+                    pred.right = curr
+                    res.append(curr.val)
+                    curr = curr.left
+                else:
+                    pred.right = None
+                    curr = curr.right
         return res
+        
+#         res = []
+#         def preorder(root):
+#             if root:
+#                 res.append(root.val)
+#                 preorder(root.left)
+#                 preorder(root.right)                
+                
+#         preorder(root)
+#         return res
                 
