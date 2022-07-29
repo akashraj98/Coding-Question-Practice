6ba8e43c314092aeb9085cac735b2edd0b43cfe3
Akash Raj <akashraj7713@gmail.com>
2022-07-29 15:13:48 +0530

    Time: 612 ms (55.36%), Space: 61.5 MB (28.80%) - LeetHub

--- 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.py
+++ 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.py
@@ -7,13 +7,13 @@
 class Solution:
     def longestZigZag(self, root: Optional[TreeNode]) -> int:
         def f(isleft,depth,root):
-            if not root:
-                return depth-1
-            else:
+            if root:
                 if isleft:
                     return max(f(1,1,root.left),f(0,depth+1,root.right))
                 else:
                     return max(f(1,depth+1,root.left),f(0,1,root.right))
+            else: 
+                return depth-1
 
             
         return f(0,0,root)
