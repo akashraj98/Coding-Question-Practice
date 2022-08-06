38a1de09e7b7fd2f6c98d5edce16bbd6f83c053d
Akash Raj <akashraj7713@gmail.com>
2022-08-06 17:09:45 +0530

    Time: 68 ms (97.78%), Space: 14.2 MB (94.72%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.py
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.py
@@ -9,22 +9,42 @@ class Solution:
         """
         Do not return anything, modify root in-place instead.
         """
-        curr = []
+        self.first = None
+        self.second = None
+        self.prev = TreeNode(-(2<<31+1))
+        # [6,3,4,5,2]
         def dfs(root):
             if root:
                 dfs(root.left)
-                curr.append(root.val)
+                if not self.first and root.val < self.prev.val:
+                    self.first = self.prev
+                if self.first and root.val < self.prev.val:
+                    self.second = root
+                self.prev = root
+                    
+                # do the business
                 dfs(root.right)
         dfs(root)
-        curr.sort()
-        self.i= 0
-        def dfs2nd(root):
-            if root:
-                dfs2nd(root.left)
-                if root.val != curr[self.i]:
-                    root.val = curr[self.i]
-                self.i+=1
-                dfs2nd(root.right)
-        dfs2nd(root)
+        self.first.val,self.second.val = self.second.val,self.first.val
+
+        
+        
+#         curr = []
+#         def dfs(root):
+#             if root:
+#                 dfs(root.left)
+#                 curr.append(root.val)
+#                 dfs(root.right)
+#         dfs(root)
+#         curr.sort()
+#         self.i= 0
+#         def dfs2nd(root):
+#             if root:
+#                 dfs2nd(root.left)
+#                 if root.val != curr[self.i]:
+#                     root.val = curr[self.i]
+#                 self.i+=1
+#                 dfs2nd(root.right)
+#         dfs2nd(root)
         
 
