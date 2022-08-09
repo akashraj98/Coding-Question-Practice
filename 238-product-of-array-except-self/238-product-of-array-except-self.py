9cab1696e963c733810b812898d36d4f6eb6a92e
Akash Raj <akashraj7713@gmail.com>
2022-08-09 12:57:17 +0530

    Time: 350 ms (53.85%), Space: 21.4 MB (44.65%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -1,36 +1,36 @@
 class Solution:
-    def productExceptSelf(self, A: List[int]) -> List[int]:
-        
-        c_zero = 0
-        mul = 1
-        indx_zero = 0
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+#         c_zero = 0
+#         mul = 1
+#         indx_zero = 0
+#         res = []
+#         for index,val in enumerate(A):
+#             if val == 0:
+#                 c_zero+=1
+#                 indx_zero = index  
+#                 continue
+#             mul = mul*val
+
+#         if c_zero > 1:
+#             return [0]*len(A)
+#         if c_zero ==1:
+#             ans = [0]*len(A)
+#             ans[indx_zero] = mul
+#             return ans
+
+#         for i in A:
+#             if i == 0:
+#                 res.append(mul)
+#             res.append(mul//i)
+#         return res
+        prefix = 1  # first prefix will always be 1
         res = []
-        for index,val in enumerate(A):
-            if val == 0:
-                c_zero+=1
-                indx_zero = index  
-                continue
-            mul = mul*val
-
-        if c_zero > 1:
-            return [0]*len(A)
-        if c_zero ==1:
-            ans = [0]*len(A)
-            ans[indx_zero] = mul
-            return ans
-
-        for i in A:
-            if i == 0:
-                res.append(mul)
-            res.append(mul//i)
+        for i in range(len(nums)):
+            res.append(prefix)
+            prefix = prefix* nums[i]
+        postfix =1  # fist post fix from last will always be 1
+        
+        for j in range(len(res)-1,-1,-1):
+            res[j]= res[j]* postfix
+            postfix = postfix* nums[j]
         return res
-        # p = 1
-        # out = []
-        # for i in range(len(nums)):
-        #     out.append(p)
-        #     p = p* nums[i]
-        # p=1
-        # for j in range(len(out)-1,-1,-1):
-        #     out[j] = p* out[j]
-        #     p = p* nums[j]
-        # return out
