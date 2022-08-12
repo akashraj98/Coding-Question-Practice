2ba19dc7f5447e45c8031e09008454a32b74f30b
Akash Raj <akashraj7713@gmail.com>
2022-08-12 10:26:37 +0530

    Time: 31 ms (99.99%), Space: 18.6 MB (90.53%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.py
+++ 110-balanced-binary-tree/110-balanced-binary-tree.py
@@ -10,7 +10,9 @@ class Solution:
             if not root:
                 return 0
             l = dfs(root.left)
+            if l == -1: return -1
             r = dfs(root.right)
+            if r == -1: return -1
             if l == -1 or r == -1 or abs(l-r)>1:
                 return -1
             return 1+max(l,r)
