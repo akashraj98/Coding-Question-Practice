1f354157206192a7b72631e55990b722c4945794
Akash Raj <akashraj7713@gmail.com>
2022-08-12 22:22:06 +0530

    Time: 400 ms (32.19%), Space: 15.7 MB (64.18%) - LeetHub

--- 503-next-greater-element-ii/503-next-greater-element-ii.py
+++ 503-next-greater-element-ii/503-next-greater-element-ii.py
@@ -3,10 +3,10 @@ class Solution:
         n = len(nums)
         res = [-1]* n
         stack = []
-        for _ in [0,1]:
-            for i in range(n):
-                while stack and nums[stack[-1]] < nums[i]:
-                    index = stack.pop()
-                    res[index] = nums[i]
-                stack.append(i)
+        # for _ in [0,1]:       # constant so it time complexity is O(n)
+        for i in range(2*n):
+            while stack and nums[stack[-1]] < nums[i%n]:
+                index = stack.pop()
+                res[index] = nums[i%n]
+            stack.append(i%n)
         return res
