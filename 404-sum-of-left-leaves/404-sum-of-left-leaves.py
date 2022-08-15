9dd013a6fa090f5549bf77d927d37a8b048817e0
Akash Raj <akashraj7713@gmail.com>
2022-08-16 00:06:13 +0530

    Time: 37 ms (88.59%), Space: 14.3 MB (77.48%) - LeetHub

--- 404-sum-of-left-leaves/404-sum-of-left-leaves.py
+++ 404-sum-of-left-leaves/404-sum-of-left-leaves.py
@@ -6,10 +6,24 @@
 #         self.right = right
 class Solution:
     def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
-        def dfs(root,isLeft):
-            if root:
-                if not root.left and not root.right and isLeft:
-                    return root.val
-                return dfs(root.left,True)+ dfs(root.right,False)
-            return 0
-        return dfs(root,False)
+        q = deque([(root,False)])
+        l_sum = 0
+        while q:
+            for i in range(len(q)):
+                node,isLeft = q.popleft()
+                if isLeft and not node.left and not node.right:
+                    l_sum+= node.val
+                if node.left:
+                    q.append((node.left,True))
+                if node.right:
+                    q.append((node.right,False))                
+        return l_sum
+    
+    
+        # def dfs(root,isLeft):
+        #     if root:
+        #         if not root.left and not root.right and isLeft:
+        #             return root.val
+        #         return dfs(root.left,True)+ dfs(root.right,False)
+        #     return 0
+        # return dfs(root,False)
