0212505ef890daba35aec52158f08ae3ec893afe
Akash Raj <akashraj7713@gmail.com>
2022-08-16 17:50:04 +0530

    Time: 71 ms (5.54%), Space: 13.8 MB (98.20%) - LeetHub

--- 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.py
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.py
@@ -5,23 +5,30 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    val = 0
     def bstToGst(self, root: TreeNode) -> TreeNode:
-        def dfs(node):
-            if node:
-                dfs(node.left)
-                visited.append(node.val)
-                dfs(node.right)
-        def dfs1(node):
-            if node:
-                dfs1(node.left)
-                node.val= next(itervisit)
-                dfs1(node.right)
-        visited = []
+        if root:
+            self.bstToGst(root.right)
+            root.val = self.val = root.val+self.val
+            self.bstToGst(root.left)
+            return root
+#         def dfs(node):
+#             if node:
+#                 dfs(node.left)
+#                 visited.append(node.val)
+#                 dfs(node.right)
+#         def dfs1(node):
+#             if node:
+#                 dfs1(node.left)
+#                 node.val= next(itervisit)
+#                 dfs1(node.right)
+#         visited = []
         
-        dfs(root)
-        for i in range(len(visited)-2,-1,-1):
-            visited[i] = visited[i] + visited[i+1]
-        itervisit = iter(visited)
-        dfs1(root)
-        return root
+#         dfs(root)
+#         print(visited)
+#         for i in range(len(visited)-2,-1,-1):
+#             visited[i] = visited[i] + visited[i+1]
+#         itervisit = iter(visited)
+#         dfs1(root)
+#         return root
             
