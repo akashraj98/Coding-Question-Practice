b3169b6439b1c63697be7bff7967df3c66c8d446
Akash Raj <akashraj7713@gmail.com>
2022-08-18 18:23:27 +0530

    Time: 113 ms (99.40%), Space: 16.1 MB (45.17%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -6,16 +6,15 @@ class Solution:
         r_max = 0
         area = 0
         while left < right:
-            if height[left] <= height[right]:  # since right bar is greater so 
-                if height[left] >= l_max:     # the water level in b/w is deter mine by left bar which is small
+            if height[left] < height[right]:
+                if height[left] >= l_max:
                     l_max = height[left]
                 else:
                     area+= l_max - height[left]
                 left+=1
-            else:                         #since left bar is greater so water 
-                if height[right] >= r_max:# level is determine by right bar which is smaller
-                    r_max = height[right]  #for curr position  if r_bar is greater than max right bar till now then no water can be filler 
-                                           # else water can be filled in curr positon with level max_r - height[r_bar]
+            else:
+                if height[right] >= r_max:
+                    r_max = height[right]
                 else:
                     area += r_max - height[right]
                 right-=1
