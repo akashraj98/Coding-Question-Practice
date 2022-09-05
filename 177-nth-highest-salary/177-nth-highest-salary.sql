40740b765853e4a1f8179bc806aca19c2a6039b1
Akash Raj <akashraj7713@gmail.com>
2022-09-05 12:28:16 +0530

    Time: 490 ms (44.02%), Space: 0B (100.00%) - LeetHub

---
+++ 177-nth-highest-salary/177-nth-highest-salary.sql
@@ -0,0 +1,11 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+SET N = N-1;
+  RETURN (
+      select ifnull((select distinct salary
+      from Employee
+      order by salary desc
+      limit 1 offset N), NULL)
+      
+  );
+END
