f3addb8c64b968f207ab347e40b949406cdd72d7
Akash Raj <akashraj7713@gmail.com>
2022-09-05 12:15:03 +0530

    Time: 311 ms (43.79%), Space: 0B (100.00%) - LeetHub

--- 176-second-highest-salary/176-second-highest-salary.sql
+++ 176-second-highest-salary/176-second-highest-salary.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 SELECT 
-(SELECT distinct salary  
+IFNULL((SELECT distinct salary  
 FROM Employee 
 order by Salary 
-desc limit 1 offset 1) AS SecondHighestSalary
+desc limit 1 offset 1),NULL) AS SecondHighestSalary
