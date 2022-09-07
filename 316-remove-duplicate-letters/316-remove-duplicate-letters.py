6b6a9792a0490ad17d518a3e16fb59982e0ff0a5
Akash Raj <akashraj7713@gmail.com>
2022-09-07 22:11:39 +0530

    Time: 65 ms (35.54%), Space: 14 MB (39.32%) - LeetHub

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.py
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.py
@@ -1,16 +1,35 @@
 class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
-        count= Counter(s)
+        last_occr = {}
+        visited = set()
         stack = []
-        seen = set()
-        
-        for c in s:
-            count[c]-=1  # don;t forget this
-            if c in seen:
+        for i,el in enumerate(s):
+            last_occr[el] = i
+
+        for i in range(len(s)):
+            if s[i] in visited:
                 continue
-            while stack and c < stack[-1] and count[stack[-1]] > 0:
-                elem = stack.pop()
-                seen.remove(elem)
-            stack.append(c)
-            seen.add(c)
-        return "".join(stack)
+            while stack and stack[-1]> s[i] and last_occr[stack[-1]] > i:
+                visited.remove(stack.pop())
+                    
+            stack.append(s[i])
+            visited.add(s[i])
+        
+        return ''.join(stack)
+        
+        
+#     Counter method Apprach 2        
+#         count= Counter(s)
+#         stack = []
+#         seen = set()
+        
+#         for c in s:
+#             count[c]-=1  # don;t forget this
+#             if c in seen:
+#                 continue
+#             while stack and c < stack[-1] and count[stack[-1]] > 0:
+#                 elem = stack.pop()
+#                 seen.remove(elem)
+#             stack.append(c)
+#             seen.add(c)
+#         return "".join(stack)
