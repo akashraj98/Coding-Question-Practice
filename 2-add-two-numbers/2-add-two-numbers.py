a18fe83fc98c98fb808f6c9f695a4d54c1e3e2f7
Akash Raj <akashraj7713@gmail.com>
2022-09-09 17:15:24 +0530

    Time: 154 ms (9.04%), Space: 13.9 MB (86.41%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -7,30 +7,44 @@ class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         carry = 0
         dummy = node = ListNode(0)
-        while l1 and l2:
-            q =l1.val+l2.val + carry
-            carry, q = divmod(q,10)
+        
+        while l1 or l2:
+            l1val = l1.val if l1 else 0
+            l2val = l2.val if l2 else 0
+            carry,q = divmod(l1val+l2val+carry,10)
             node.next = ListNode(q)
             node = node.next
-            l1 = l1.next
-            l2 = l2.next
-        while l1:
-            q = l1.val+carry
-            carry, q = divmod(q,10)
-            node.next = ListNode(q)
-            node = node.next
-            l1 = l1.next
-            
-        while l2:
-            q = l2.val+carry
-            carry, q = divmod(q,10)
-            node.next = ListNode(q)
-            node = node.next
-            l2 = l2.next
-            
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
         if carry:
             node.next = ListNode(carry)
-            
         return dummy.next
+            
+            
+#         while l1 and l2:
+#             q =l1.val+l2.val + carry
+#             carry, q = divmod(q,10)
+#             node.next = ListNode(q)
+#             node = node.next
+#             l1 = l1.next
+#             l2 = l2.next
+#         while l1:
+#             q = l1.val+carry
+#             carry, q = divmod(q,10)
+#             node.next = ListNode(q)
+#             node = node.next
+#             l1 = l1.next
+            
+#         while l2:
+#             q = l2.val+carry
+#             carry, q = divmod(q,10)
+#             node.next = ListNode(q)
+#             node = node.next
+#             l2 = l2.next
+            
+#         if carry:
+#             node.next = ListNode(carry)
+            
+#         return dummy.next
         
         
