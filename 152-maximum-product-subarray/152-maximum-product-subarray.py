8e61d585d1bd58294d51f255251db250d37c9099
Akash Raj <akashraj7713@gmail.com>
2022-09-16 21:25:58 +0530

    Time: 210 ms (6.63%), Space: 14.5 MB (38.13%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.py
+++ 152-maximum-product-subarray/152-maximum-product-subarray.py
@@ -1,9 +1,6 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        #Modified kadane algorithm
-        # at each iteration we check max and min prod using suffix and prefix arr
-        # we take max of suffix, prefix, currtmax and update our max
-        # in case of zero we have to handle it by taking 1 insted 0 pref or suf
+
         
         prefix, sufix, currmax = 0,0,float("-inf")
         for i in range(len(nums)):
