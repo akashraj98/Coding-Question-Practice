a899da277a0f822853835c85d432b13ff7f757bb
Akash Raj <akashraj7713@gmail.com>
2022-09-16 17:16:58 +0530

    Time: 163 ms (10.16%), Space: 13.8 MB (99.17%) - LeetHub

--- 445-add-two-numbers-ii/445-add-two-numbers-ii.py
+++ 445-add-two-numbers-ii/445-add-two-numbers-ii.py
@@ -31,13 +31,13 @@ class Solution:
         return prev
 
 
-# carry,head = 0,None
-#         while l1_s or l2_s or carry:
-#             n1 = l1_s.pop().val if l1_s  else 0
-#             n2 = l2_s.pop().val if l2_s  else 0
-#             carry,q = divmod(n1+n2+carry,10)
-#             head = ListNode(q,head)
-#         return head
-            
+#     carry,head = 0,None
+#     while l1_s or l2_s or carry:
+#         n1 = l1_s.pop().val if l1_s  else 0
+#         n2 = l2_s.pop().val if l2_s  else 0
+#         carry,q = divmod(n1+n2+carry,10)
+#         head = ListNode(q,head)
+#     return head
+
             
             
