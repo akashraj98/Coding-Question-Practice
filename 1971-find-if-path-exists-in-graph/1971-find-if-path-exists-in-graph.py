03f97e0f98fbf783f5be30d89cfd33d03be33f89
Akash Raj <akashraj7713@gmail.com>
2022-09-20 13:26:10 +0530

    Time: 4251 ms (15.13%), Space: 106.4 MB (58.40%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,30 +1,43 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        
+        neighbors = defaultdict(list)
+        for n1,n2 in edges:
+            neighbors[n1].append(n2)
+            neighbors[n2].append(n1)
 
-        vis = set()
-        adjnode = defaultdict(list)
-        for edge in edges:
-            adjnode[edge[0]].append(edge[1])
-            adjnode[edge[1]].append(edge[0])
-        def dfs(node,end,vis):
-            if node == end: return True
-            if node in vis: return False
-            vis.add(node)
-            for adj in adjnode[node]:
-                if dfs(adj,end,vis):
-                    return True
-            return False
-        return dfs(source,destination,vis)
+        vis =set([source])
+        q = deque([source])
+        while q:
+            node = q.popleft()
+            print(node)
+            if node == destination:
+                return True
+            for adj in neighbors[node]:
+                if adj not in vis:
+                    q.append(adj)
+                    vis.add(adj)
+        return False
+            
+
         
+#         if n-1 ==0: #Corner case
+#             return True
+#         vis = set()
+#         # This solution is valid but will give memory exceed for larger case
+#         node_no = 1+max([l1[0] for l1 in edges]+ [l1[1] for l1 in edges])
+#         adjmatrix = [[0]*node_no for _ in range(node_no)]
+#         for i,j in edges:
+#             adjmatrix[i][j] = adjmatrix[j][i] = 1
 #         vis.add(source)
 #         q = deque([source])
 #         while q:
 #             node=q.popleft()
-#             if node == destination: return True
-#             for adj in adjnode[node]:
-#                 if adj not in vis:
+#             if node == destination:
+#                 return True
+#             for adj in range(node_no):
+#                 if adjmatrix[node][adj] and adj not in vis:
 #                     q.append(adj)
 #                     vis.add(adj)
+
 #         return False
                     
