c02b1446e115521e512a209d7820d713875c6a6e
Akash Raj <akashraj7713@gmail.com>
2022-09-28 14:18:12 +0530

    Time: 25 ms (99.66%), Space: 13.9 MB (70.33%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
@@ -14,6 +14,7 @@ class Solution:
             if count == n:
                 prev = head
             curr = curr.next
+            
         if prev:
             prev.next = prev.next.next
         else:
