0c8817e3dd2f29f30b654873cf00703565e68a44
Akash Raj <akashraj7713@gmail.com>
2022-10-08 12:17:57 +0530

    Time: 4300 ms (8.04%), Space: 14.8 MB (47.53%) - LeetHub

--- 732-my-calendar-iii/732-my-calendar-iii.py
+++ 732-my-calendar-iii/732-my-calendar-iii.py
@@ -1,16 +1,20 @@
 from sortedcontainers import SortedDict
-
-
 class MyCalendarThree:
 
     def __init__(self):
-        self.diff = SortedDict()
+        self.dict = SortedDict()
 
     def book(self, start: int, end: int) -> int:
-        self.diff[start] = self.diff.get(start, 0) + 1
-        self.diff[end] = self.diff.get(end, 0) - 1
-        cur = res = 0
-        for delta in self.diff.values():
-            cur += delta
-            res = max(cur, res)
+        self.dict[start] = self.dict.get(start,0)+1
+        self.dict[end] = self.dict.get(end,0)-1
+        res=curr = 0
+        for val in self.dict.values():
+            curr+=val
+            res = max(res,curr)
         return res
+        
+
+
+# Your MyCalendarThree object will be instantiated and called as such:
+# obj = MyCalendarThree()
+# param_1 = obj.book(start,end)
