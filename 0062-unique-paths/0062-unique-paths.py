cf0715fd55058dc07f5c45eee1f18e489b536c83
Akash Raj <akashraj7713@gmail.com>
2022-10-14 03:08:53 +0530

    Time: 48 ms (67.43%), Space: 14 MB (31.98%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,10 +1,9 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int: 
-        dp = [[-1]*(m) for i in range(n)]
-        print(dp)
+        dp = [[-1]*(n) for i in range(m)]
         def countPath(i,j,m,n):
-            if i == (n-1) and j == (m-1): return 1
-            if i >=n or j>=m: return 0
+            if i == (m-1) and j == (n-1): return 1
+            if i >=m or j>=n: return 0
             if dp[i][j] != -1: return dp[i][j]
             else:
                 dp[i][j] = countPath(i+1,j,m,n) + countPath(i,j+1,m,n)
