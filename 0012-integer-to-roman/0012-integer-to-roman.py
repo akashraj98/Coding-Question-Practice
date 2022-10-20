4eaa7814455963784cc51be6bb8f9a5b42d71c98
Akash Raj <akashraj7713@gmail.com>
2022-10-20 12:52:45 +0530

    Time: 48 ms (96.08%), Space: 14 MB (6.54%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -0,0 +1,22 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        hmap = {1:"I", 4:"IV",
+                5:"V", 9:"IX", 10:"X",
+                40:"XL", 50:"L", 90:"XC", 100:"C",
+                400:"CD", 500:"D",900:"CM", 1000:"M"
+               }
+        def findPrev(n):
+            prev = 0
+            for k in hmap:
+                if k<=n:
+                    prev = k
+                else:
+                    return prev
+            return prev
+        res = ""
+        while num:
+            prev = findPrev(num)
+            prev_freq = num //prev
+            num = num% prev
+            res = res+ hmap[prev]* prev_freq
+        return res
