b009a589b5796056ddce04b4642c6581b20ccb7b
Akash Raj <akashraj7713@gmail.com>
2022-10-28 18:45:31 +0530

    Time: 99 ms (96.72%), Space: 17.8 MB (67.00%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,7 +1,16 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        hm = defaultdict(list)
-        for s in strs:
-            key = "".join(sorted(s))
-            hm[key].append(s)
-        return list(hm.values())
+        h_map = {}
+        res = []
+        for i in strs:
+            si = "".join(sorted(i))
+            if si in h_map:
+                h_map[si] += [i]
+            else:
+                h_map[si] = [i]
+                
+        # for k,v in h_map.items():
+        #     res.append(v)
+        return h_map.values()
+            
+        
