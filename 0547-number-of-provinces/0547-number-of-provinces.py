6a4d3fc4a68cdda74cac8079d911c439fbf24a7f
Akash Raj <akashraj7713@gmail.com>
2023-03-04 13:54:31 +0530

    Time: 173 ms (99.21%), Space: 14.1 MB (94.32%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        vis = set()
+        count = 0
+        n = len(isConnected)
+        for i in range(n):
+            if i in vis:
+                continue
+            count+=1
+            vis.add(i)
+            q = deque([i]) # everytime we need to make ques
+            #since we will visit all nodes that can be visited from i
+            while q:
+                el= q.pop()
+                for adj in range(n):
+                    if isConnected[el][adj]:
+                        if adj not in vis:
+                            vis.add(adj)
+                            q.append(adj)
+        return count
