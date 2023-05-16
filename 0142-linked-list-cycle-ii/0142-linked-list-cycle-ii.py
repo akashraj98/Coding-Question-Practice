2b5a94aff64c15a7b63fc73c6691fe2b43b4de0d
Akash Raj <akashraj7713@gmail.com>
2023-05-17 02:55:01 +0530

    Time: 64 ms (26.07%), Space: 20.7 MB (8.76%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
@@ -11,7 +11,7 @@ class Solution:
             slow =slow.next
             fast = fast.next.next
             if slow == fast:
-                slow = head
+                fast = head
                 while slow!=fast:
                     slow = slow.next
                     fast = fast.next
