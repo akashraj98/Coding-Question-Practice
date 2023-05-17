3693f7b1f5cac51becda1770f7d262f76263d65f
Akash Raj <akashraj7713@gmail.com>
2023-05-17 21:13:58 +0530

    Added solution - LeetHub

---
+++ Flattening a Linked List - GFG/flattening-a-linked-list.py
@@ -0,0 +1,108 @@
+#User function Template for python3
+
+
+'''
+
+class Node:
+    def __init__(self, d):
+        self.data=d
+        self.next=None
+        self.bottom=None
+        
+'''
+def mergeLL(a,b):
+    res = dummy = Node(0)
+    while a and b:
+        if a.data<b.data:
+            dummy.bottom =a
+            dummy = dummy.bottom
+            a = a.bottom
+        else:
+            dummy.bottom = b
+            dummy = dummy.bottom
+            b = b.bottom
+    if a:
+        dummy.bottom = a
+    if b:
+        dummy.bottom =b
+    return res.bottom
+
+def flatten(root):
+    #Your code here
+    if root is None or root.next is None:
+        return root
+    root.next=flatten(root.next)
+    root = mergeLL(root,root.next)
+    return root
+   
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+class Node:
+    def __init__(self, d):
+        self.data=d
+        self.next=None
+        self.bottom=None
+        
+        
+
+def printList(node):
+    while(node is not None):
+        print(node.data,end=" ")
+        node=node.bottom
+        
+    print()
+
+
+if __name__=="__main__":
+    t=int(input())
+    while(t>0):
+        head=None
+        N=int(input())
+        arr=[]
+        
+        arr=[int(x) for x in input().strip().split()]
+        temp=None
+        tempB=None
+        pre=None
+        preB=None
+        
+        flag=1
+        flag1=1
+        listo=[int(x) for x in input().strip().split()]
+        it=0
+        for i in range(N):
+            m=arr[i]
+            m-=1
+            a1=listo[it]
+            it+=1
+            temp=Node(a1)
+            if flag == 1:
+                head=temp
+                pre=temp
+                flag=0
+                flag1=1
+            else:
+                pre.next=temp
+                pre=temp
+                flag1=1
+                
+            for j in range(m):
+                a=listo[it]
+                it+=1
+                tempB=Node(a)
+                if flag1 == 1:
+                    temp.bottom=tempB
+                    preB=tempB
+                    flag1=0
+                else:
+                    preB.bottom=tempB
+                    preB=tempB
+        root=flatten(head)
+        printList(root)
+        
+        t-=1
+            
+# } Driver Code Ends
