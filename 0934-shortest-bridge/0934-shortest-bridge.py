2fe75ac341d6cd8493514844ef73f62081ef92a1
Akash Raj <akashraj7713@gmail.com>
2023-05-21 10:26:12 +0530

    Time: 413 ms (51.29%), Space: 20.9 MB (18.06%) - LeetHub

--- 0934-shortest-bridge/0934-shortest-bridge.py
+++ 0934-shortest-bridge/0934-shortest-bridge.py
@@ -2,6 +2,7 @@ class Solution:
     def shortestBridge(self, grid: List[List[int]]) -> int:
         # Find first island
         # Convert it to 2s and push it in queue
+    
         n = len(grid)
         f_x,f_y = 0,0
         for i in range(n):
@@ -9,12 +10,16 @@ class Solution:
                 if grid[i][j]==1:
                     f_x,f_y = i,j
                     break
-        
+        def inrange(cur_x,cur_y):
+            return 0 <= cur_x < n and 0 <= cur_y < n
+        def nextfour(x,y):
+            return [(x+1,y),(x,y+1),(x-1,y),(x,y-1)]
+            
         def dfs(x,y):
             grid[x][y]=2
             bfs_queue.append((x,y))
-            for cur_x,cur_y in [(x+1,y),(x,y+1),(x-1,y),(x,y-1)]:
-                if 0 <= cur_x < n and 0 <= cur_y < n and grid[cur_x][cur_y] == 1:
+            for cur_x,cur_y in nextfour(x,y):
+                if inrange(cur_x,cur_y) and grid[cur_x][cur_y] == 1:
                     dfs(cur_x, cur_y)
         bfs_queue = []
         dfs(f_x,f_y)
@@ -22,8 +27,8 @@ class Solution:
         while bfs_queue:
             new_bfs=[]
             for x,y in bfs_queue:
-                for cur_x,cur_y in [(x+1,y),(x,y+1),(x-1,y),(x,y-1)]:
-                    if 0 <= cur_x < n and 0 <= cur_y < n :
+                for cur_x,cur_y in nextfour(x,y):
+                    if inrange(cur_x,cur_y):
                         if grid[cur_x][cur_y] ==1:
                             return distance
                         elif grid[cur_x][cur_y]==0:
