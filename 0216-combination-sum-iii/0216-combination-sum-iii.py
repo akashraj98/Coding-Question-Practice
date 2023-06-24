285c77515822bb0871a03be9c9c2019900cda8b0
Akash Raj <akashraj7713@gmail.com>
2023-06-24 19:50:31 +0530

    Time: 39 ms (93.32%), Space: 16.4 MB (46.97%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.py
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -1,16 +1,15 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
-        def backtrack(nums,subset,res,target):
-            if len(subset)==k and target==0:
+        def backtrack(nums,subset,res):
+            if len(subset)==k and sum(subset)==n:
                 res.append(subset)
                 return
 
             for i in range(len(nums)):
-                if nums[i]>target:
-                    break
-                backtrack(nums[i+1:],subset+[nums[i]],res,target-nums[i])
+
+                backtrack(nums[i+1:],subset+[nums[i]],res)
                 
         nums = [i+1 for i in range(9)]
         result = []
-        backtrack(nums,[],result,n)
+        backtrack(nums,[],result)
         return result
